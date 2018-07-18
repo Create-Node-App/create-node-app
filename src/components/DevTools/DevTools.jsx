694f6efd2d8b73eefc9d8470260c1e35cf8346b8
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-18 19:57:36 -0300

    Updated monitors

--- src/components/DevTools/DevTools.jsx
+++ src/components/DevTools/DevTools.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
+import Dispatcher from 'redux-devtools-dispatch';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
+import LogMonitor from 'redux-devtools-log-monitor';
+import SliderMonitor from 'redux-slider-monitor';
 
 export default createDevTools(
   <DockMonitor
@@ -9,6 +13,9 @@ export default createDevTools(
     changePositionKey="ctrl-alt-w"
     changeMonitorKey='ctrl-alt-m'
   >
+    <ChartMonitor />
     <LogMonitor />
+    <FilterableLogMonitor />
+    <SliderMonitor />
   </DockMonitor>
 );
