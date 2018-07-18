f9672269044eb8f072ff2736aec2b3d92141a919
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-18 19:58:09 -0300

    Updated monitors

--- src/components/DevTools/DevTools.jsx
+++ src/components/DevTools/DevTools.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import ChartMonitor from 'redux-devtools-chart-monitor';
-import Dispatcher from 'redux-devtools-dispatch';
 import DockMonitor from 'redux-devtools-dock-monitor';
 import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
 import LogMonitor from 'redux-devtools-log-monitor';
