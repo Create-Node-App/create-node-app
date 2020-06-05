f8cafa90192d5fcb7bb09983febcf72de62bd315
ulises-jeremias <ulisescf.24@gmail.com>
2020-06-05 06:07:57 -0300

    Added typescript support

---
+++ config/typescript/babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      "env",
+      {
+        targets: {
+          node: "12.14"
+        }
+      }
+    ],
+    "@babel/preset-react",
+    "@babel/preset-env",
+    "@babel/typescript"
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+    "react-hot-loader/babel",
+    [
+      "module-resolver",
+      {
+        root: [
+          "."
+        ],
+        alias: {
+          app: "./src"
+        }
+      }
+    ]
+  ]
+};
