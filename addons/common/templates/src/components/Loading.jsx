4ff3bc8c771c8124e6eaf0aa399b0302b7d2d557
ulises-jeremias <ulisescf.24@gmail.com>
2020-06-06 02:42:26 -0300

    Added loading component

---
+++ addons/common/templates/src/components/Loading.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Loading = () => <></>;
+
+export default Loading;
---
+++ addons/semantic-ui/templates/src/components/Loading.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+const Loading = () => <Dimmer inverted active><Loader /></Dimmer>;
+
+export default Loading;
