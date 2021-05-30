9c81d851a5d13f8055e944249c91967b0b48e675
ulises-jeremias <ulisescf.24@gmail.com>
2021-05-29 23:04:16 -0300

    Updated addons

--- src/addons.js
+++ src/addons.js
@@ -4,6 +4,10 @@ module.exports = (options) => {
   // initialized with base template
   let addons = [];
 
+  if (options.template) {
+    addons = [{ addon: options.template }];
+  }
+
   if (options.extend) {
     addons.push(...options.extend.filter(Boolean).map((addon) => ({ addon, git: true })));
   }
