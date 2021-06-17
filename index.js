525bf8b1a2673b1712f11574136cfce7a0948cd3
ulises-jeremias <ulisescf.24@gmail.com>
2021-06-17 11:28:18 -0300

    Updated execution

--- index.js
+++ index.js
@@ -9,37 +9,6 @@ const { createApp } = require('./lib/install');
 const { getCnaOptions } = require('./cna');
 const { Options } = require('./docs');
 
-let projectName = 'app';
-
-program
-  .version(packageJS.version)
-  .arguments('[project-directory]')
-  .usage(`${chalk.green('[project-directory]')} [options]`)
-  .action((name) => {
-    projectName = name || projectName;
-  })
-  .option('--verbose', 'print additional logs')
-  .option('--info', 'print environment debug info')
-  .option('--use-npm', 'use npm mandatorily')
-  .option('-i, --interactive', 'use interactive mode to bootstrap your app')
-  .option('--template <template>', 'specify template to use for initial setup')
-  .option('--extend <repos...>', 'git repositories to extend your boilerplate')
-  .option('-a, --alias <alias>', 'webpack alias', 'app')
-  .option('--src-dir <src-dir>', 'dir name to put content under [src]/', '')
-  .option('--nodeps', 'generate package.json file without installing dependencies')
-  .option('--inplace', 'apply setup to an existing project');
-
-program
-  .allowUnknownOption()
-  .on('--help', () => {
-    console.log();
-    console.log(`    Only ${chalk.green('[project-directory]')} is required.`);
-    console.log();
-    console.log(`    If you have any problems, do not hesitate to file an issue:`);
-    console.log(`      ${chalk.cyan(`${packageJS.bugs.url}/new`)}`);
-  })
-  .parse(process.argv);
-
 const printEnvInfo = async () => {
   console.log(chalk.bold('\nEnvironment Info:'));
   const info = await envinfo.run(
@@ -95,6 +64,37 @@ const cna = async (options, transformOptions = getCnaOptions) => {
 };
 
 if (require.main === module) {
+  let projectName = 'app';
+
+  program
+    .version(packageJS.version)
+    .arguments('[project-directory]')
+    .usage(`${chalk.green('[project-directory]')} [options]`)
+    .action((name) => {
+      projectName = name || projectName;
+    })
+    .option('--verbose', 'print additional logs')
+    .option('--info', 'print environment debug info')
+    .option('--use-npm', 'use npm mandatorily')
+    .option('-i, --interactive', 'use interactive mode to bootstrap your app')
+    .option('--template <template>', 'specify template to use for initial setup')
+    .option('--extend <repos...>', 'git repositories to extend your boilerplate')
+    .option('-a, --alias <alias>', 'webpack alias', 'app')
+    .option('--src-dir <src-dir>', 'dir name to put content under [src]/', '')
+    .option('--nodeps', 'generate package.json file without installing dependencies')
+    .option('--inplace', 'apply setup to an existing project');
+
+  program
+    .allowUnknownOption()
+    .on('--help', () => {
+      console.log();
+      console.log(`    Only ${chalk.green('[project-directory]')} is required.`);
+      console.log();
+      console.log(`    If you have any problems, do not hesitate to file an issue:`);
+      console.log(`      ${chalk.cyan(`${packageJS.bugs.url}/new`)}`);
+    })
+    .parse(process.argv);
+
   cna({ ...program.opts(), projectName });
 }
 
